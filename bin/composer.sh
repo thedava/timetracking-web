f3a03dc6e9e1fee2301c1f0a378a37461b523633
thedava <thedava@users.noreply.github.com>
2016-12-28 18:31:25 +0100

    Updated binaries

---
+++ bin/composer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://getcomposer.org/installer -O composer-setup.php
+php composer-setup.php
+rm -f composer-setup.php
--- bin/phpcs.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (!file_exists('php-cs-fixer.phar')) {
-    $content = file_get_contents('https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v1.12.3/php-cs-fixer.phar');
-    file_put_contents('php-cs-fixer.phar', $content);
-}
